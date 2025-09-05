54a98aa6e4a1d0221a39a5593c7f1ec2b4454563
seunfolly <folahan@umweltdev.com>
2025-09-05 17:13:37 +0100

    feat(frontend): update App.jsx and remove backend files
    - Updated App.jsx with latest changes
    - Removed backend controllers from frontend repo
    - Resolved .env.production merge conflict

--- backend/utils/randomGenerator.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-
-const generateRandomHex = () => {
-  const buffer = crypto.randomBytes(4);
-  return "#" + buffer.toString("hex");
-}
-
-module.exports = generateRandomHex;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,26 @@ import { useEffect, useRef, useState } from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Routes, Route, Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
+
 import Homepage from "./pages/homepage";
 import Store from "./pages/store";
 import UserDashBoard from "./pages/user-dashboard/index";
 import AdminDashboard from "./pages/admin-dashboard";
 import ProductDescription from "./pages/product-description";
-import "@fortawesome/fontawesome-free/css/all.css";
 import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import ResetPassword from "./pages/auth/ResetPassword";
 import Cart from "./pages/cart";
-import { useDispatch, useSelector } from "react-redux";
-import { userCart, logout, resetState } from "./features/auth/authSlice";
-import { getUserCart } from "./features/cart/cartSlice";
 import NotFound from "./pages/NotFound";
-import { Box, Typography } from "@mui/material";
+
+import "@fortawesome/fontawesome-free/css/all.css";
+import { Box } from "@mui/material";
+
+import { useDispatch, useSelector } from "react-redux";
+import { userCart, logout } from "./features/auth/authSlice";
+import { getUserCart } from "./features/cart/cartSlice";
 
 function PrivateRoute({ children, authorizedRoles }) {
   const user = useSelector((state) => state.auth.user);
@@ -25,22 +29,21 @@ function PrivateRoute({ children, authorizedRoles }) {
   return isAuthorized ? <>{children}</> : <Navigate to="/login" />;
 }
 
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+  authorizedRoles: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
 function App() {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
-  const [isLoading, setIsLoading] = useState(true);
   const { user } = useSelector((state) => state.auth);
-
+  const [isLoading, setIsLoading] = useState(true);
   const isInitialMount = useRef(true);
 
   useEffect(() => {
-    const loadingTimeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-
-    return () => {
-      clearTimeout(loadingTimeout);
-    };
+    const timer = setTimeout(() => setIsLoading(false), 3000);
+    return () => clearTimeout(timer);
   }, []);
 
   useEffect(() => {
@@ -48,103 +51,93 @@ function App() {
       isInitialMount.current = false;
       return;
     }
-    // if (user && cart.products && cart.products.length > 0) {
     if (user && cart.products) {
-      const data = {
-        cart: cart.products,
-      };
-      dispatch(userCart(data));
+      dispatch(userCart({ cart: cart.products }));
     }
-  }, [dispatch, cart]);
+  }, [dispatch, cart, user]);
 
   useEffect(() => {
-    if (user) {
-      dispatch(getUserCart());
-    }
-  }, [user]);
+    if (user) dispatch(getUserCart());
+  }, [dispatch, user]);
 
-  const getUserfromLocalStorage = localStorage.getItem("user");
   useEffect(() => {
     let logoutTimer;
+    const storedUser = localStorage.getItem("user");
 
-    if (getUserfromLocalStorage) {
-      const userData = JSON.parse(getUserfromLocalStorage);
-      if (user && userData.expirationTime) {
+    if (storedUser) {
+      const userData = JSON.parse(storedUser);
+      if (userData.expirationTime) {
         const remainingTime =
           new Date(userData.expirationTime).getTime() - new Date().getTime();
 
         if (remainingTime <= 0) {
           dispatch(logout());
         } else {
-          logoutTimer = setTimeout(() => {
-            return dispatch(logout());
-          }, remainingTime);
+          logoutTimer = setTimeout(() => dispatch(logout()), remainingTime);
         }
       }
-    } else {
-      clearTimeout(logoutTimer);
     }
-  }, [dispatch, getUserfromLocalStorage, user]);
 
-  return (
-    <>
-      {isLoading ? (
-        <Box
-          sx={{
-            width: "100vw",
-            height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <img
-            src="https://bazaar.ui-lib.com/assets/images/bazaar-black-sm.svg"
-            style={{
-              animation: "scaleAnimation 2s infinite",
-            }}
-          />
-        </Box>
-      ) : (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <Routes>
-            <Route exact path="/" element={<Homepage />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/product/:id" element={<ProductDescription />} />
+    return () => clearTimeout(logoutTimer);
+  }, [dispatch, user]);
 
-            <Route
-              path="/admin/*"
-              element={
-                <PrivateRoute authorizedRoles={["admin"]}>
-                  <AdminDashboard />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path="/user/*"
-              element={
-                <PrivateRoute authorizedRoles={["admin", "user"]}>
-                  <UserDashBoard />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="*" element={<NotFound />} />
-            <Route
-              path="/cart"
-              element={
-                <PrivateRoute authorizedRoles={["admin", "user"]}>
-                  <Cart />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </LocalizationProvider>
-      )}
-    </>
+  return isLoading ? (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img
+        src="https://bazaar.ui-lib.com/assets/images/bazaar-black-sm.svg"
+        style={{ animation: "scaleAnimation 2s infinite" }}
+        alt="Loading..."
+      />
+    </Box>
+  ) : (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/product/:id" element={<ProductDescription />} />
+
+        <Route
+          path="/admin/*"
+          element={
+            <PrivateRoute authorizedRoles={["admin"]}>
+              <AdminDashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/user/*"
+          element={
+            <PrivateRoute authorizedRoles={["admin", "user"]}>
+              <UserDashBoard />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoute authorizedRoles={["admin", "user"]}>
+              <Cart />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </LocalizationProvider>
   );
 }
 
